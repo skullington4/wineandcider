5a3d981e293a2974faa5e5f931c6f6adc93c32f3
Kevin <ksully121888@gmail.com>
2024-03-27 14:37:10 -0400

    First push

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,6 +6,7 @@ import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
+import Home from '../Home/Home';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -17,6 +18,7 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
+              <Route path="/" element={<Home />} />
               <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
             </Routes>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function Home({}) {
+
+  return (
+    <main>
+        <h1>Home Page</h1>
+
+    </main>
+  );
+}
